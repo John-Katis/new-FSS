7daf1bfd250636c5c8cbb602c9bad96e3dba5b45
Jannis Katis <john13katis@gmail.com>
2023-12-09 11:50:06 +0100

    Fixed mask and u calculation

--- pika/libmpc/src/protocols/pika.rs
+++ pika/libmpc/src/protocols/pika.rs
@@ -11,46 +11,38 @@ pub const TOTAL_BITS:usize = 32;
 
 pub async fn pika_eval(p: &mut MPCParty<BasicOffline>, x_share:&RingElm)->RingElm{
     let mut ret = RingElm::zero();
+    
     // Protocol 2(a) - reconstruct x=r-a(mod2^k) -> r: random val, a: secret sharing of user input
-    // TODO check quantization correctness
     let quantized_x_share = x_share.quantize_16();
     let party_mask = p.offlinedata.r_share[0].wrapping_sub(quantized_x_share);
     let mask = p.netlayer.exchange_u16_vec(p.offlinedata.r_share.to_vec()).await;
     let mut x = mask[0];
 
-    // TODO reduce x_share to u16 before calculations
-    x = x.wrapping_sub(10u16);
-
     // Protocol 2(b) - compute yσ (EvalAll routine -> implement in DPF key)
     let y_vec = p.offlinedata.k_share[0].evalAll();
-    // println!("y_vec LENGTH {:?}",y_vec.len());
+    println!("y_vec LENGTH {:?}",y_vec.len());
 
     let func_database = load_func_db(); // -> load works but store is not done correctly -> load 32 files
-    // println!("FUNC DB LENGTH {}", func_database.len());
+    println!("FUNC DB LENGTH {}", func_database.len());
 
     let mut u: RingElm = RingElm::from(0u32);
     
-    // TODO wrapping add index
     // Protocol 2(c) - compute u
-    // FIXME shift_index needs to wrap around 2^k
-    // for i in 0..y_vec.len() {
-    //     // println!("STEP 2C PROGRESS: {}", i);
-    //     // TODO do this in plaintext - i+x.usize
-    //     let mut ring_shift_index = RingElm::from(i as u32) + x;
-    //     let usize_shift_index = ring_shift_index.to_usize();
+    for i in 0..y_vec.len() {
+        let mut shift_index: u16 = i as u16;
+        shift_index = shift_index.wrapping_add(x);
 
-    //     // println!("--- STEP 2C PROGRESS: {}", usize_shift_index);
-    //     if y_vec[usize_shift_index] {
-    //         let mut temp = RingElm::from(func_database[i]);
+        if y_vec[shift_index as usize] {
+            let mut temp = RingElm::from(func_database[i]);
             
-    //         // -1^σ
-    //         if !p.netlayer.is_server {
-    //             temp.negate();
-    //         }
+            // -1^σ
+            if !p.netlayer.is_server {
+                temp.negate();
+            }
 
-    //         u = u + temp;
-    //     }   
-    // }
+            u = u + temp;
+        }   
+    }
 
     println!("CURRENT SHARE:");
     x_share.print();
@@ -58,7 +50,9 @@ pub async fn pika_eval(p: &mut MPCParty<BasicOffline>, x_share:&RingElm)->RingEl
 
     println!("X SUBBED VALUE:");
     println!("{}", quantized_x_share);
-    println!("");
+
+    println!("MASK X VALUE:");
+    println!("{}", x);
 
     println!("U VALUE:");
     u.print();
@@ -100,15 +94,4 @@ fn load_func_db()->Vec<f32>{
         ret.append(&mut temp)
     }
     ret
-}
-
-// The function database, evalAll and calculation here must happen over 2^k (not 2^l which is the input domain)
-fn quantize_input(input_domain_x_share:&RingElm)->RingElm{
-    let mut bound_domain_x_share = RingElm::zero();
-
-    // It works by truncation
-    // A number m where k <= m <= l-2 is set based on which truncation happens
-    // But the way I understand it, they define a dynamic method of truncation (?!) but the bounded domain is always 2^k (k=9? or 16?)
-
-    bound_domain_x_share
 }
